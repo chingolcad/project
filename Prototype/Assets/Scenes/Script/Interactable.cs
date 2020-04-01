3328de03b5eb56fc2cf148e53960437c3ab0a83e
Chi Ngo <chingo@lcad.edu>
2020-04-01 16:36:14 -0700

    prototype in unity. problems with navmesh agent.

---
+++ Prototype/Assets/Scenes/Script/Interactable.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Interactable : MonoBehaviour
+{
+    public float radius = 3f;
+    public NavMeshAgent playerAgent;
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+    public virtual void MoveToInteraction(NavMeshAgent playerAgent)
+    {
+        this.playerAgent = playerAgent;
+        playerAgent.destination = this.transform.position;
+
+        Interact();
+    }
+
+public virtual void Interact()
+    {
+        Debug.Log("Interacting with base class.");
+    }
+}
---
+++ Prototype/Assets/Scenes/Script/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    public Interactable focus;
+    public LayerMask movementMask;
+
+    Camera cam;
+    PlayerMotor motor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                motor.MovetoPoint(hit.point);
+
+                //move our player to what we hit
+                // remove focus
+                RemoveFocus();
+
+            }
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                Interactable interactable = hit.collider.GetComponent<Interactable>();
+                if (interactable != null)
+                {
+                    SetFocus(interactable);
+                }
+            }
+        }
+    }
+    void SetFocus (Interactable newFocus)
+    {
+        focus = newFocus;
+        motor.FollowTarget(newFocus);
+    }
+
+    void RemoveFocus ()
+    {
+        focus = null;
+        motor.StopFollowingTarget();
+    }
+    
+}
